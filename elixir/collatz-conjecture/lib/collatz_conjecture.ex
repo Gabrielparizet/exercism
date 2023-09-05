ce48f9e43a3fcc8b5dcae2c10a5f07b524ad5ac1
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-05 15:33:42 +0200

    feat(collatz_conjecture): do_calc for even and odd numbers

---
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,26 @@
+defmodule CollatzConjecture do
+  require Integer
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(0) do
+    raise ArgumentError, message: "0 is invalid"
+  end
+  def calc(num) do
+    do_calc(num, 0)
+  end
+  def do_calc(1, step) do
+    step
+  end
+  def do_calc(num, step) do
+    if Integer.is_even(num) do
+      do_calc(div(num, 2), step + 1)
+    else
+      do_calc(num * 3 +1, step + 1)
+    end
+  end
+end
