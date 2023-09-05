660c82c6cd4da04810320663fad512d97e5f209d
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-05 17:32:39 +0200

    feat(space-age): All tests are valid

---
+++ elixir/space-age/lib/space_age.ex
@@ -0,0 +1,34 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet', or an error if 'planet' is not a planet.
+  """
+  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+
+  @planets_earth_years %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+    @earth_year_in_seconds 31557600
+
+  def age_on(planet, seconds) when is_map_key(@planets_earth_years, planet) do
+    {:ok, seconds / (@earth_year_in_seconds * @planets_earth_years[planet])}
+  end
+end
