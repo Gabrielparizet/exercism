70e9284824e2c4ed220bee66934ec870f198af72
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-05 16:24:18 +0200

    fix(collatz_conjecture): Refactor calc without FunctionClauseError and If Else statement

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -7,20 +7,23 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(0), do: raise FunctionClauseError
-  def calc(num) when num < 0, do: raise FunctionClauseError
-  def calc(num) when is_binary(num), do: raise FunctionClauseError
-  def calc(num) do
-    do_calc(num, 0)
-  end
-  def do_calc(1, step) do
-    step
-  end
-  def do_calc(num, step) do
-    if Integer.is_even(num) do
-      do_calc(div(num, 2), step + 1)
-    else
-      do_calc(num * 3 +1, step + 1)
-    end
-  end
+  # def calc(0), do: raise FunctionClauseError
+  # def calc(num) when num < 0, do: raise FunctionClauseError
+  # def calc(num) when is_binary(num), do: raise FunctionClauseError
+  # def calc(num) do
+  #   do_calc(num, 0)
+  # end
+  # def do_calc(1, step) do
+  #   step
+  # end
+  # def do_calc(num, step) do
+  #   if Integer.is_even(num) do
+  #     do_calc(div(num, 2), step + 1)
+  #   else
+  #     do_calc(num * 3 +1, step + 1)
+  #   end
+  # end
+  def calc(1), do: 0
+  def calc(i) when i > 0 and rem(i, 2) != 0, do: 1 + calc(i * 3 + 1)
+  def calc(i) when i > 0 and rem(i, 2) == 0, do: 1 + calc(div(i, 2))
 end
