d16b7da132f5ca6237a3de5e7e318e5c02a093d8
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-11 13:34:54 +0200

    feat(binary-search): Finds a valye in the middle of a tuple

---
+++ elixir/binary-search/lib/binary_search.ex
@@ -0,0 +1,40 @@
+defmodule BinarySearch do
+  require Integer
+  @doc """
+    Searches for a key in the tuple using the binary search algorithm.
+    It returns :not_found if the key is not in the tuple.
+    Otherwise returns {:ok, index}.
+
+    ## Examples
+
+      iex> BinarySearch.search({}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 5)
+      {:ok, 2}
+
+  """
+
+  @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search(numbers, key) do
+    numbers_list = Tuple.to_list(numbers)
+    cond do
+      find_middle_index(numbers_list) == key ->
+        {:ok,
+        numbers_list
+      |> length()
+      |> div(2)
+    }
+    end
+  end
+
+  def find_middle_index(numbers) do
+    middle_index = numbers
+    |> length()
+    |> div(2)
+    Enum.at(numbers, middle_index)
+  end
+end
