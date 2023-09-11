087294a4526f6c17b8b6fb6ebab61ad17ede43e1
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-11 15:54:55 +0200

    feat(german-sysadmin): All tests are validated, ready for first submit

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -1,5 +1,6 @@
 defmodule BinarySearch do
   require Integer
+
   @doc """
     Searches for a key in the tuple using the binary search algorithm.
     It returns :not_found if the key is not in the tuple.
@@ -21,20 +22,25 @@ defmodule BinarySearch do
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
     numbers_list = Tuple.to_list(numbers)
+
     cond do
-      find_middle_index(numbers_list) == key ->
+      find_middle_index_value(numbers_list) == key ->
         {:ok,
-        numbers_list
-      |> length()
-      |> div(2)
-    }
+         numbers_list
+         |> length()
+         |> div(2)}
+
+       true ->
+        "It is greater"
     end
   end
 
-  def find_middle_index(numbers) do
-    middle_index = numbers
-    |> length()
-    |> div(2)
+  def find_middle_index_value(numbers) do
+    middle_index =
+      numbers
+      |> length()
+      |> div(2)
+
     Enum.at(numbers, middle_index)
   end
 end
--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -3,8 +3,12 @@ defmodule Username do
 
   def sanitize([head | tail]) do
     case head do
-      head when (head >= 97 and head <= 122) -> [head]
-      head when (head >= 48 and head <= 57) -> []
+      head when (head >= 97 and head <= 122) or head == 95 -> [head]
+      head when head == 228 -> [97, 101]
+      head when head == 246 -> [111, 101]
+      head when head == 252 -> [117, 101]
+      head when head == 223 -> [115, 115]
+      _ -> []
 
     end ++ sanitize(tail)
 
