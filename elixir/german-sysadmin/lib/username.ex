216c224891dd0774312c4902e12e4ffa1823e39e
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-11 16:01:51 +0200

    feat(german-sysadmin): sanitize refactored without using code points for readability

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -3,20 +3,13 @@ defmodule Username do
 
   def sanitize([head | tail]) do
     case head do
-      head when (head >= 97 and head <= 122) or head == 95 -> [head]
-      head when head == 228 -> [97, 101]
-      head when head == 246 -> [111, 101]
-      head when head == 252 -> [117, 101]
-      head when head == 223 -> [115, 115]
+      head when (head >= ?a and head <= ?z) or head == ?_ -> [head]
+      head when head == ?ä -> [?a, ?e]
+      head when head == ?ö -> [?o, ?e]
+      head when head == ?ü -> [?u, ?e]
+      head when head == ?ß -> [?s, ?s]
       _ -> []
 
     end ++ sanitize(tail)
-
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
   end
 end
