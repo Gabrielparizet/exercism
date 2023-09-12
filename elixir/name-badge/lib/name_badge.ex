a4fe0a81ac215f1bfef280b0be7d4bbad91e3273
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-12 13:35:24 +0200

    feat(name-badge) All tests are valid.

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,9 +1,17 @@
 defmodule NameBadge do
   def print(id, name, department) do
-      if id == nil do
-        "#{name} - #{String.upcase(department)}"
+      if id == nil and department == nil do
+        "#{name} - OWNER"
       else
-      "[#{id}] - #{name} - #{String.upcase(department)}"
+        if department == nil do
+          "[#{id}] - #{name} - OWNER"
+        else
+          if id == nil do
+            "#{name} - #{String.upcase(department)}"
+          else
+            "[#{id}] - #{name} - #{String.upcase(department)}"
+          end
+        end
       end
   end
 end
