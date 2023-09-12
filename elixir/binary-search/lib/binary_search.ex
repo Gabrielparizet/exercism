d00427a4ed96a9db4179357793acdf6ba0f6ffcd
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-12 12:05:45 +0200

    feat(binary-search): All tests are passed, ready to submit

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -1,5 +1,4 @@
 defmodule BinarySearch do
-  require Integer
 
   @doc """
     Searches for a key in the tuple using the binary search algorithm.
@@ -23,40 +22,25 @@ defmodule BinarySearch do
   def search(numbers, key) do
     numbers_list = Tuple.to_list(numbers)
     binary_search(numbers_list, key, 0)
-
   end
 
   defp binary_search([], _key, _index), do: :not_found
   defp binary_search(numbers_list, key, index) do
-    middle_index = find_middle_index(numbers_list)
-    middle_index_value = find_middle_index_value(numbers_list, middle_index)
-    # IO.inspect(key, label: "the key we are looking for")
-    # IO.inspect(numbers_list, label: "the numbers list")
-    # IO.inspect(middle_index, label: "the middle index")
-    # IO.inspect(middle_index_value, label: "the middle index value")
-    case middle_index_value do
-      middle_index_value when middle_index_value == key -> {:ok, middle_index}
-      middle_index_value when middle_index_value < key -> binary_search(delete_smaller_elements_in_list(numbers_list, middle_index), key, index + middle_index - 1)
-      middle_index_value when middle_index_value > key -> binary_search(delete_bigger_elements_in_list(numbers_list, middle_index), key, index + middle_index + 1)
+    list_length = length(numbers_list)
+    middle_index = div(list_length, 2)
+    middle_value = Enum.at(numbers_list, middle_index)
+
+    case key do
+      key when key == middle_value -> {:ok, index + middle_index}
+      key when key < middle_value ->
+        new_list = Enum.take(numbers_list, middle_index)
+        binary_search(new_list, key, index)
+      key when key > middle_value ->
+        new_list = Enum.drop(numbers_list, middle_index + 1)
+        binary_search(new_list, key, index + middle_index + 1)
+      _ ->
+        :not_found
     end
-  end
-
-  def find_middle_index(numbers_list) do
-    numbers_list
-    |> length()
-    |> div(2)
-  end
-
-  def find_middle_index_value(numbers_list, index) do
-    Enum.at(numbers_list, index)
-  end
-
-  def delete_smaller_elements_in_list(numbers_list, middle_index) do
-    Enum.take(numbers_list, -middle_index)
-  end
-
-  def delete_bigger_elements_in_list(numbers_list, middle_index) do
-    Enum.take(numbers_list, middle_index)
   end
 
 
