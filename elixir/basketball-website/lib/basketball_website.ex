80dffb0d3f7479902a09d8057a79b9670d050146
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-12 17:09:19 +0200

    feat(BasketballWebsite): extract_from_path passed all the test

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,8 +1,15 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
     # Please implement the extract_from_path/2 function
-    data[path]
+    keys = String.split(path, ".")
+    do_extract_from_path(data, keys)
   end
+
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [first_key | rest]) do
+    do_extract_from_path(data[first_key], rest)
+  end
+
 
   def get_in_path(data, path) do
     # Please implement the get_in_path/2 function
