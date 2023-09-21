a881ddfb53bdfc40060cc3476b4077aeb12fbfaf
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-21 11:04:44 +0200

    feat(boutique-inventory): with_missing_price/1 passes all tests

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,10 +1,10 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    Enum.sort_by(inventory, &(&1.price), :asc)
+    Enum.sort_by(inventory, fn x -> x.price end, :asc)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    Enum.reject(inventory, fn x -> x.price end)
   end
 
   def update_names(inventory, old_word, new_word) do
