88a6594daa98b420911950b3f07952c42612f53e
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-21 17:41:58 +0200

    feat(boutique-inventory): total_quantity/1 passes all the tests

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -8,14 +8,24 @@ defmodule BoutiqueInventory do
   end
 
   def update_names(inventory, old_word, new_word) do
-    Enum.map(inventory, fn item -> %{item | name: String.replace(item.name, old_word, new_word)} end)
+    # Enum.map(inventory, fn item -> %{item | name: String.replace(item.name, old_word, new_word)} end)
+    # Enum.map(inventory, fn item -> Map.put(item, :name, String.replace(item.name, old_word, new_word)) end)
+    # Enum.map(inventory, fn item -> change_names(item, old_word, new_word) end)
+    Enum.map(inventory, &(change_names(&1, old_word, new_word)))
+  end
+
+  defp change_names(item, old_word, new_word) do
+    Map.put(item, :name, String.replace(item.name, old_word, new_word))
   end
 
   def increase_quantity(item, count) do
     %{item | quantity_by_size: Map.new(item.quantity_by_size, fn {key, value} -> {key, value + count} end)}
   end
 
+
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item.quantity_by_size
+    |> Map.values()
+    |> Enum.reduce(0, fn x, acc -> x + acc end)
   end
 end
