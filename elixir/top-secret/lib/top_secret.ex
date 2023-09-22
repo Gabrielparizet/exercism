8a405c1b11c9b17aa2400922d6aee4aee9ae91c2
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-22 15:17:49 +0200

    feat(top-secret): decode_secret_message_part passes all tests for pt.2

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -3,9 +3,17 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
+  def decode_secret_message_part({operation, _, list_of_arguments} = ast, acc)
+    when operation in [:def, :defp], do: {ast, [ get_list_of_arguments(list_of_arguments) | acc ]}
+
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    {ast, acc}
   end
+
+    defp get_list_of_arguments(list_of_arguments) do
+      {operation, _, _args} = Enum.at(list_of_arguments, 0)
+      Atom.to_string(operation)
+    end
 
   def decode_secret_message(string) do
     # Please implement the decode_secret_message/1 function
