b48ef83fed6395644b66683c3602756d308df345
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-25 12:13:51 +0200

    feat(top-secret): All tests are passed

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -3,19 +3,31 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
-  def decode_secret_message_part({operation, _, list_of_arguments} = ast, acc)
-    when operation in [:def, :defp], do: {ast, [ get_name(list_of_arguments) | acc ]}
-
+  def decode_secret_message_part({op, _, args} = ast, acc) when op in [:def, :defp] do
+    {function_name, function_args} = get_function_name_and_args(args)
+    arity = length(function_args)
+    message = String.slice(to_string(function_name), 0, arity)
+    {ast, [message | acc]}
+  end
   def decode_secret_message_part(ast, acc) do
     {ast, acc}
   end
-
-    defp get_name(list_of_arguments) do
-      {operation, _, _args} = Enum.at(list_of_arguments, 0)
-      Atom.to_string(operation)
+  defp get_function_name_and_args(def_args) do
+    case def_args do
+      [{:when, _, args} | _] -> get_function_name_and_args(args)
+      [{name, _, args} | _] when is_list(args) -> {name, args}
+      [{name, _, args} | _] when is_atom(args) -> {name, []}
     end
+  end
+
+
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    ast_node = to_ast((string))
+    {_, acc} = Macro.prewalk(ast_node, [], &decode_secret_message_part/2)
+
+    acc
+    |> Enum.reverse()
+    |> Enum.join("")
   end
 end
