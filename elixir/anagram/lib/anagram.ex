dc1f71f1d436fb40ad0e34a3c94d467500cc2e27
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-26 14:24:50 +0200

    feat(anagram): All tests are valid

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -2,36 +2,19 @@ defmodule Anagram do
   @doc """
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
-  base = "stone"
-  candidates = ["stone", "tones", "banana", "tons", "notes", "Seton"]
-
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
-    sorted_base = sort_strings(base)
-    sorted_candidates = Enum.map(candidates, &(sort_strings(&1)))
-    matching_indexes = find_matching_indexes(sorted_candidates, sorted_base)
-    matching_list = find_matching_list(matching_indexes, candidates)
-    Enum.reject(matching_list, &(&1 == base))
+    candidates
+    |> Enum.filter(fn elem -> anagram(base, elem) end)
   end
-
-  def sort_strings(string) do
-    string
+  defp anagram(base, candidate) do
+    (String.downcase(base) != String.downcase(candidate)) and
+    (homog(base) == homog(candidate))
+  end
+  defp homog(word) do
+    word
     |> String.downcase()
-    |> String.codepoints()
+    |> String.to_charlist()
     |> Enum.sort()
-    |> Enum.join()
   end
-
-  def find_matching_indexes(list, comparing_value) do
-    list
-    |> Enum.with_index()
-    |> Enum.filter(fn {value, _index} -> String.equivalent?(value, comparing_value) end)
-    |> Enum.map(fn {_value, index} -> index end)
-  end
-
-  def find_matching_list(list_of_indexes, list_of_strings) do
-    list_of_indexes
-    |> Enum.map(&(Enum.at(list_of_strings, &1)))
-  end
-
 end
