5969ef63ec1e50ecf9b52c59e340a25d3a7b42db
Gabrielparizet <parizetgabriel@gmail.com>
2023-10-03 10:36:41 +0200

    feat(raindrops): convert/1 passes all tests

--- elixir/raindrops/lib/raindrops.ex
+++ elixir/raindrops/lib/raindrops.ex
@@ -9,6 +9,10 @@ defmodule Raindrops do
     just pass the number's digits straight through.
   """
   @spec convert(pos_integer) :: String.t()
+  def convert(number) when rem(number, 3) == 0 and rem(number, 5) == 0 and rem(number, 7) == 0, do: "PlingPlangPlong"
+  def convert(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "PlingPlang"
+  def convert(number) when rem(number, 3) == 0 and rem(number, 7) == 0, do: "PlingPlong"
+  def convert(number) when rem(number, 5) == 0 and rem(number, 7) == 0, do: "PlangPlong"
   def convert(number) when rem(number, 3) == 0, do: "Pling"
   def convert(number) when rem(number, 5) == 0, do: "Plang"
   def convert(number) when rem(number, 7) == 0, do: "Plong"
