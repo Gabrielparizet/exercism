8d103b46244eac3ec45ed8a81b89a748304dbbc9
Gabrielparizet <parizetgabriel@gmail.com>
2023-10-05 17:32:38 +0200

    feat(nucleotide): historigram/1 passes all the tests

---
+++ elixir/nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,36 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count(~c"AATAA", ?A)
+  4
+
+  iex> NucleotideCount.count(~c"AATAA", ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) do
+    Enum.count(strand, fn x -> x == nucleotide end)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram(~c"AATAA")
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    a = count(strand, ?A)
+    c = count(strand, ?C)
+    g = count(strand, ?G)
+    t = count(strand, ?T)
+    %{?A => a, ?T => t, ?C => c, ?G => g}
+  end
+end
