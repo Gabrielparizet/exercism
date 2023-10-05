86a94afa26c851c7ca7908b639e9ce99c1def3f7
Gabrielparizet <parizetgabriel@gmail.com>
2023-10-05 18:16:58 +0200

    feat(grade_school): all tests are passed before checking for grades

---
+++ elixir/grade-school/lib/school.ex
@@ -0,0 +1,43 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @type school :: any()
+
+  @doc """
+  Create a new, empty school.
+  """
+  @spec new() :: school
+  def new() do
+    []
+  end
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(school, String.t(), integer) :: {:ok | :error, school}
+  def add(school, name, _grade) do
+    case Enum.member?(school, name) do
+      true  -> {:error, school}
+      false -> {:ok, school ++ [name]}
+    end
+  end
+
+  @doc """
+  Return the names of the students in a particular grade, sorted alphabetically.
+  """
+  @spec grade(school, integer) :: [String.t()]
+  def grade(school, grade) do
+  end
+
+  @doc """
+  Return the names of all the students in the school sorted by grade and name.
+  """
+  @spec roster(school) :: [String.t()]
+  def roster(school) do
+    school
+  end
+end
