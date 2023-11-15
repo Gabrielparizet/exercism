2d7ad77221da9ef14f752af5932d4946103fb267
Gabrielparizet <parizetgabriel@gmail.com>
2023-11-15 18:13:09 +0100

    feat(etl): Etl for comprehension exercises started

---
+++ elixir/etl/lib/etl.ex
@@ -0,0 +1,16 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.map(input, fn x ->
+      for {points, letters} <- x, do: %{points => List.to_string(String.downcase(letters))}
+    end)
+  end
+end
