0bfd7902dce9b6829d58ab9aac4253d29bdef828
Gabrielparizet <parizetgabriel@gmail.com>
2023-11-22 11:08:51 +0100

    feat(lucas-numbers): generate/1 passes all the tests

---
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -0,0 +1,32 @@
+defmodule LucasNumbers do
+  @moduledoc """
+  Lucas numbers are an infinite sequence of numbers which build progressively
+  which hold a strong correlation to the golden ratio (φ or ϕ)
+
+  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
+  """
+
+  # SOLUTION WITH ENUMERABLES
+  # def generate(count) when not is_integer(count) or count < 1 do
+  #   raise(ArgumentError, "count must be specified as an integer >= 1")
+  # end
+  # def generate(1), do: [2]
+  # def generate(2), do: [2, 1]
+  # def generate(count) do
+  #   previous_list = generate(count - 1)
+  #   next_number = previous_list
+  #   |> Enum.take(-2)
+  #   |> Enum.reduce(0, fn x, acc -> x + acc end)
+  #   Enum.reverse([next_number | Enum.reverse(previous_list)])
+  # end
+
+
+  # SOLUTION WITH STREAMS
+  def generate(count) when is_integer(count) and count >= 1 do
+    Stream.unfold({2, 1}, fn {a, b} -> {a, {b, a + b}} end)
+    |> Enum.take(count)
+  end
+  def generate(_) do
+    raise(ArgumentError, "count must be specified as an integer >= 1")
+  end
+end
