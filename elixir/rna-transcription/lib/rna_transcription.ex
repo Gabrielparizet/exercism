cdd851f48b9cecff4a9649936c84fcea2a6020c8
Gabrielparizet <parizetgabriel@gmail.com>
2023-11-23 14:34:24 +0100

    feat(RnaTranscription): to_rna/1 passess all the tests

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,23 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+    iex> RnaTranscription.to_rna(~c"ACTG")
+    ~c"UGAC"
+  """
+
+  @dna_to_rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, &(@dna_to_rna[&1]))
+  end
+
+end
