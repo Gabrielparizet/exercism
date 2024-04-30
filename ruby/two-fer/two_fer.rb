33e77831e6463252016764d6a5bfc4c7df02e0bb
Gabrielparizet <parizetgabriel@gmail.com>
2024-04-30 17:50:25 +0200

    two_fer

---
+++ ruby/two-fer/two_fer.rb
@@ -0,0 +1,9 @@
+=begin
+Write your code for the 'Two Fer' exercise in this file. Make the tests in
+`two_fer_test.rb` pass.
+=end
+class TwoFer
+    def self.two_fer(name = "you")
+        "One for #{name}, one for me."
+    end
+end
