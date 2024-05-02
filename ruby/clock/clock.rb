bb64209cb89df37b69682eff60b354d318eff06c
Gabrielparizet <parizetgabriel@gmail.com>
2024-05-02 17:06:46 +0200

    wip: clock

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,14 @@
+=begin
+Write your code for the 'Clock' exercise in this file. Make the tests in
+`clock_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/clock` directory.
+=end
+class ClockTest 
+    def initialize(hour)
+        @hour = hour
+    end
+
+    
+end
