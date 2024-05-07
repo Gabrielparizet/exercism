0158453efcfc356cf5d200e17336d9af20a1cfb4
Gabrielparizet <parizetgabriel@gmail.com>
2024-05-07 13:59:32 +0200

    WIP: hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,14 @@
+=begin
+Write your code for the 'Hamming' exercise in this file. Make the tests in
+`hamming_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/hamming` directory.
+=end
+class Hamming
+
+    def self.compute(strand_1, strand_2)
+        strand_1.
+    end
+
+end
