c2dac300b45e7c892c66d3fab4f0ff7ab7e73695
Gabrielparizet <parizetgabriel@gmail.com>
2024-05-13 16:05:31 +0200

    hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -8,7 +8,11 @@ To get started with TDD, see the `README.md` file in your
 class Hamming
 
     def self.compute(strand_1, strand_2)
-        strand_1.
+        if strand_1.length != strand_2.length
+            raise ArgumentError 
+        else 
+            strand_1.chars.zip(strand_2.chars).count { |str1, str2| str1 != str2}
+        end
     end
 
 end
