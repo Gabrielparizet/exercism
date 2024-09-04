d106b0a82b2d0c4e049f8e04b35a3be9c4e3ae2b
Gabrielparizet <parizetgabriel@gmail.com>
2024-09-04 21:29:32 +0200

    feat: elyses enchantments

--- javascript/elyses-enchantments/enchantments.js
+++ javascript/elyses-enchantments/enchantments.js
@@ -48,7 +48,8 @@ export function insertItemAtTop(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItem(cards, position) {
-  throw new Error('Implement the removeItem function');
+  cards.splice(position, 1);
+  return cards;
 }
 
 /**
@@ -59,7 +60,8 @@ export function removeItem(cards, position) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemFromTop(cards) {
-  throw new Error('Implement the removeItemFromTop function');
+  cards.pop();
+  return cards;
 }
 
 /**
@@ -71,7 +73,8 @@ export function removeItemFromTop(cards) {
  * @returns {number[]} the cards including the new card
  */
 export function insertItemAtBottom(cards, newCard) {
-  throw new Error('Implement the insertItemAtBottom function');
+  cards.unshift(newCard);
+  return cards;
 }
 
 /**
@@ -82,7 +85,8 @@ export function insertItemAtBottom(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemAtBottom(cards) {
-  throw new Error('Implement the removeItemAtBottom function');
+  cards.shift();
+  return cards;
 }
 
 /**
@@ -94,5 +98,5 @@ export function removeItemAtBottom(cards) {
  * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
  */
 export function checkSizeOfStack(cards, stackSize) {
-  throw new Error('Implement the checkSizeOfStack function');
+  return cards.length == stackSize;
 }
